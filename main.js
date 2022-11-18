886e9e2467cec1483ad1cd93e9136cca8d11f0b4
Pavel <pavel.holod2001@gmail.com>
2022-11-18 22:22:40 +0300

    somethingchange

--- main.js
+++ main.js
@@ -20,9 +20,30 @@ function select(event) {
     }
     const billValue = Number(billInput.value)
     const peopleValue = Number(peopleInput.value)
+    
+    const zeroTitle = document.querySelector('.title-red')
+    if (billValue === 0 || billValue === undefined || billValue === '') {
+        zeroTitle.style.display = 'block'
+        billInput.focus()
+        billInput.classList.add('red-outline')
+        return false
+    } else {
+        zeroTitle.style.display = 'none'
+    }
+
+    const zeroTitleBottom = document.querySelector('.title-red-bottom')
+    if (peopleValue === 0 || peopleValue === undefined || peopleValue === '') {
+        zeroTitleBottom.style.display = 'block'
+        peopleInput.focus()
+        peopleInput.classList.add('red-outline')
+        return false
+    } else {
+        zeroTitleBottom.style.display = 'none'
+    }
+
+
     const tip = event.target.closest('.panel-item')
     const num = tip.innerText.slice(0, -1)
-    console.log(num)
     const tipAmount = billValue * (num * 0.01) / peopleValue
     const sumPerson = tipAmount + billValue / peopleValue
     personTip.innerHTML = `$${tipAmount.toFixed(2)}`
